eedba02492d520e9620c152aa865d280008c734d
Lepisma <hugo.diaz.morcillo@gmail.com>
2023-07-16 16:22:44 +0200

    Upgrade tree test
    A class for generating a tree structure for  researching upgrades, auto-generates a valid tree shape from all the loaded upgrades

--- Assets/Scripts/Utilities/UpgradeTreeTest.cs
+++ Assets/Scripts/Utilities/UpgradeTreeTest.cs
@@ -8,29 +8,44 @@ using Frontiers.Content;
 using Frontiers.Content.Upgrades;
 
 public class UpgradeTreeTest : MonoBehaviour {
-    TreeNode[] nodes;
-    TreeNode masterNode;
+    UpgradeTree upgradeTree;
+
+    public Sprite nodeSprite;
+    public Material nodeLineMaterial;
+
+    public static UpgradeTreeTest instance;
 
     private void Awake() {
+        instance = this;
+
         AssetLoader.LoadAssets();
         ContentLoader.LoadContents();
 
-        CreateNodes();
-        InstantiateNodes();
+        upgradeTree = new();
+        upgradeTree.Instantiate();
+    }
+}
+
+public class UpgradeTree {
+    Node[] nodes;
+    Node masterNode;
+
+    public UpgradeTree() {
+        Generate();
     }
 
-    public void CreateNodes() {
+    public void Generate() {
         UpgradeType[] allUpgrades = UpgradeHandler.loadedUpgrades.Values.ToArray();
 
-        Dictionary<short, TreeNode> nodes = new();
+        Dictionary<short, Node> nodes = new();
         masterNode = new();
 
         for (int i = 0; i < allUpgrades.Length; i++) {
             UpgradeType upgrade = allUpgrades[i];
             UpgradeType parentUpgrade = upgrade.previousUpgrade;
 
-            TreeNode parentNode = parentUpgrade == null ? masterNode : nodes[parentUpgrade.id];
-            TreeNode currentNode = new(upgrade, parentNode);
+            Node parentNode = parentUpgrade == null ? masterNode : nodes[parentUpgrade.id];
+            Node currentNode = new(upgrade, parentNode);
 
             nodes[upgrade.id] = currentNode;
             parentNode.Add(currentNode);
@@ -39,49 +54,78 @@ public class UpgradeTreeTest : MonoBehaviour {
         this.nodes = nodes.Values.ToArray();
     }
 
-    public void InstantiateNodes() {
-        masterNode.Instantiate(Vector2.zero);
+    public void Instantiate() {
+        masterNode.Instantiate(null, Vector2.zero);
     }
 
-    private class TreeNode {
+    public class Node {
+        public const float nodeUpSpacing = 2.25f;
+        public const float nodeSideSpacing = 2f;
+        public const float nodeSize = 1f;
+
         public UpgradeType upgrade;
 
-        public List<TreeNode> nextNodes = new();
-        public TreeNode parentNode;
+        public List<Node> nextNodes = new();
+        public Node parentNode;
 
         public bool isMaster = false;
+        public float width;
 
-        public static Vector2 offset = Vector2.one;
-        public float xHalfSize;
-
-        public TreeNode(UpgradeType upgrade, TreeNode parentNode) {
+        public Node(UpgradeType upgrade, Node parentNode) {
             this.upgrade = upgrade;
             this.parentNode = parentNode;
         }
 
-        public TreeNode() {
+        public Node() {
             upgrade = null;
             parentNode = null;
             isMaster = true;
         }
 
-        public void Add(TreeNode nextNode) {
+        public void Add(Node nextNode) {
             nextNodes.Add(nextNode);
-            xHalfSize = nextNodes.Count / 2f;
+            nextNode.UpdateSpacing();
         }
 
-        public void Instantiate(Vector2 position) {
-            int nodes = nextNodes.Count;
-            float xHalfLength = offset.x * nodes / 2f;
+        public void UpdateSpacing() {
+            float totalNodeSize = 0;
 
+            // Get this node width based on children nodes
+            for (int i = 0; i < nextNodes.Count; i++) totalNodeSize += nextNodes[i].width + nodeSideSpacing;
+            width = Mathf.Max(nodeSize, totalNodeSize - nodeSideSpacing);
+
+            // If has parent, update it
+            if (parentNode != null) parentNode.UpdateSpacing();
+        }
+
+        public void Instantiate(Transform parent, Vector2 position) {
             string name = isMaster ? "master" : upgrade.name;
-            GameObject nodeGO = new(name, typeof(SpriteRenderer));
-            nodeGO.transform.position = position;
+            Transform nodeTransform = new GameObject(name, typeof(SpriteRenderer), typeof(LineRenderer)).transform;
 
-            for (int i = 0; i < nodes; i++) {
-                TreeNode node = nextNodes[i];
-                Vector2 nextPosition = new Vector2(i * offset.x - xHalfLength, offset.y) + position;
-                node.Instantiate(nextPosition);
+            nodeTransform.parent = parent;
+            nodeTransform.localPosition = position;
+
+            SpriteRenderer spriteRenderer = nodeTransform.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = UpgradeTreeTest.instance.nodeSprite;
+            spriteRenderer.sortingOrder = 1;
+
+            if (parent != null) {
+                LineRenderer lineRenderer = nodeTransform.GetComponent<LineRenderer>();
+                lineRenderer.SetPositions(new Vector3[2] { parent.transform.position, nodeTransform.position });
+                lineRenderer.material = UpgradeTreeTest.instance.nodeLineMaterial;
+            }
+
+            // Start with a side space
+            float widthProgress = width / -2f;
+
+            for (int i = 0; i < nextNodes.Count; i++) {
+                Node node = nextNodes[i];
+                Vector2 nextPosition = new((node.width / 2f) + widthProgress, nodeUpSpacing);
+
+                // Add node width and a space to the width progress;
+                widthProgress += node.width + nodeSideSpacing;
+
+                node.Instantiate(nodeTransform, nextPosition);
             }
         }
     }
